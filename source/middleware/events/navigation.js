2852552c100165f8ed1f382534369092218f2745
braadworst <royniels83@gmail.com>
2017-04-06 16:46:19 +0800

    Added content animation

--- source/middleware/events/navigation.js
+++ source/middleware/events/navigation.js
@@ -1,4 +1,11 @@
+const velocity = require('velocity-animate');
+
 module.exports = (next, relay) => {
-  console.log('events baby!');
+  const content = document.querySelector('#content');
+  if (window.location.pathname === '/') {
+    velocity(content, { marginTop : '42%' }, { easing: 'spring', duration : 1000 });
+  } else {
+    velocity(content, { marginTop : '100px' }, { easing: 'spring', duration : 1000 });
+  }
   next();
 }
