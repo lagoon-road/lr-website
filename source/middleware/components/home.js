d6574373dbeb3ed892f375757cd85aebaa422513
braadworst <royniels83@gmail.com>
2017-04-10 17:52:00 +0800

    Wrote the homepage information

--- source/middleware/components/home.js
+++ source/middleware/components/home.js
@@ -5,19 +5,27 @@ module.exports = (next, relay) => {
         <h1>Lagoon road <span>Laid back webapps</span></h1>
       </div>
       <hr>
-      <p class="standout">lagoon road is a package/methodology that takes middleware to the extreme. It wraps everything, your templates, DOM events, API calls, basically anything you can think of into a middleware function.</p>
-      <p>All middleware is connected to what we call the (Lagoon) road. A single object where you chain your middleware regardless of the environment you are running in, being it the client, web server, api server, Raspberry Pie or mobile device. The benefit of this is that it becomes very easy to reason about the flow of your application. There is only one place that calls your middleware, the Lagoon road.</p>
-      <p>Because of this centralized system it additionally becomes a breeze to share your code between environments. Routes or api calls on the client and web server. Login checks and data preparations on an api server and web server are just some examples. Sharing code is trivial even if you scale up your app.</p>
-      <p>Lagoon is not a monolithic framework that does everything out of the box. It is a simple package with only 250 lines of code. You can add extensions (regular npm packages) to mold it in a system that works for you and your team.
-      </p>
+      <p class="standout">Lagoon road helps you build webapps. It is not as low level as writing Javascript from scratch and it is not as high level as frameworks that can limit your flexibility. It sits right in the middle. It helps you to give structure to your app, lets you plug and play any package you like and doesn't get in the way to let you write code the way you like it. Lagoon road is laid back...</p>
+      <p>In Lagoon road, we turn everything in to middleware. Routing, templating, you name it. All this middleware is connected to a single object, which we call the <em>road</em>. You can picture it as a single road on an island. You can never get lost, it is either left or right. In Lagoon road it is even simpeler, it is a one way road.</p>
+      <p>Everything on the road is used by all environments that you are running, a web server, an API server, the client or even your Raspberry Pie/Aurdino and Mobile device. Because of this it becomes a breeze to share code among these environments and still keeping everything code wise DRY. Sharing routes between client and web server, have login checks for the api server and web server, sharing is how Lagoon road works best.</p>
+      <p>Lagoon road is not a monolithic framework that does everything out of the box. The core, and only mandatory package is less then 200 lines of code. You can add extensions (regular npm packages) to mold it in a system that works for you or your team. You remain in control over how to work and code.</p>
       <section class="highlight">
         <h2>Where from here?</h2>
         <p>
           <a href="/guide" class="pure-button">Examples in the <strong>guide</strong></a>
-          <a href="/reference" class="pure-button">Nuts & bolts in the <strong>reference</strong></a>
+          <a href="/reference" class="pure-button">Details in the <strong>reference</strong></a>
           <a href="/faq" class="pure-button">Answers in the <strong>FAQ</strong></a>
-          <a href="/about" class="pure-button">Backgrounds in the <strong>about</strong></a>
+        </p>
       </section>
+      <h2>Some <span class="laid-back">laid back</span> features of lagoon road</h2>
+      <ul class="features">
+        <li><span>1</span>It is modular, don't like a package, switch it for something else, it is all just npm packages, so plug and play the way you like</li>
+        <li><span>2</span>Websockets, HTTP(2), you can switch protocols without having to change the way you build your app</li>
+        <li><span>3</span>Plug in any store you like, you are not tied down to a single choice, you can even run multiple different ones next to each other</li>
+        <li><span>4</span>Share code between environments and keep it organized, even when you scale up</li>
+        <li><span>5</span>Want to hook up a Raspberry Pie or Arduino, sure thing, you can check for changes on sensory output and feed it straight into an api server, web server or straight to the browser</li>
+        <li><span>6</span>You know how middleware works? You are up and running for Lagoon road, no endless docs you have to plow through</li>
+      </ul>
     </section>
   `, 'article');
   next();
